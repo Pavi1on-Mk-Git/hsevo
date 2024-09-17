fd76d64aa1783e08f87fe5cd9add976cf3af879c
Paweł Kochański <pawilioxion@gmail.com>
2024-09-17 20:17:19 +0200

    Added missing CLONE_CARD to LordJaraxxusCard

--- include/logic/cards/LordJaraxxusCard.h
+++ include/logic/cards/LordJaraxxusCard.h
@@ -7,6 +7,8 @@ struct LordJaraxxusCard: public Card
 {
     LordJaraxxusCard(): Card("Lord Jaraxxus", 9) {}
 
+    CLONE_CARD
+
     std::vector<Game> on_play(Game& game, const std::vector<OnPlayArg>& args) override;
 };
 

e857daf8e77aceed08d1bac7b218b0386a50a0c8
Paweł Kochański <pawilioxion@gmail.com>
2024-11-02 12:22:11 +0100

    Corrected action arguments for cards targeting self

--- src/logic/cards/SingleArgSelfPlayPositionCard.cc
+++ src/logic/cards/SingleArgSelfPlayPositionCard.cc
@@ -6,6 +6,7 @@ std::vector<std::unique_ptr<PlayCardAction>> SingleArgSelfPlayPositionCard::crea
     const Game& game, unsigned hand_position
 ) const
 {
+    using enum TargetType;
     std::vector<std::unique_ptr<PlayCardAction>> play_self_actions;
 
     const unsigned minion_count = game.current_player().board.minion_count();
@@ -16,7 +17,7 @@ std::vector<std::unique_ptr<PlayCardAction>> SingleArgSelfPlayPositionCard::crea
 
     for(unsigned board_position = 0; board_position <= minion_count; ++board_position)
         play_self_actions.push_back(std::make_unique<PlayMinionAction>(
-            hand_position, mana_cost, board_position, std::vector<OnPlayArg>{board_position}
+            hand_position, mana_cost, board_position, std::vector<OnPlayArg>{ALLY_MINION, board_position}
         ));
 
     return play_self_actions;
--- src/logic/cards/TwilightDrake.cc
+++ src/logic/cards/TwilightDrake.cc
@@ -4,7 +4,7 @@
 
 std::vector<Game> TwilightDrake::on_play(Game& game, const std::vector<OnPlayArg>& args) const
 {
-    const auto position_played = std::get<unsigned>(args.at(0));
+    const auto position_played = std::get<unsigned>(args.at(1));
     auto& self = game.current_player().board.get_minion(position_played);
     const auto hand_size = game.current_player().hand.size();
     self.health += hand_size;
